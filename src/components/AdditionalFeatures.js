7035efdc5ffbeb891e09658b527ebfca440bfe96
Nadi <Second@Nadias-MacBook-Air.local>
2020-07-28 22:20:07 -0400

    mvp met

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 
+import { connect } from 'react-redux'; 
+
+
 const AdditionalFeatures = props => {
   return (
     <div className="content">
@@ -18,4 +21,13 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+
+const mapStateToProps = state => {
+  // console.log();
+  return {
+    additionalFeatures: state.additionalFeatures, 
+  };
+
+};
+
+export default connect(mapStateToProps,{})(AdditionalFeatures);
